7304a92ec1c1491d0c045a8031462f996e06a57d
ali4a <alancacik@gmail.com>
2025-07-02 18:17:26 +0300

    cat

--- main.gd
+++ main.gd
@@ -1,3 +1,13 @@
 extends Node
 
-	
+@onready var Battle = $Battle/click
+
+func _ready():
+	Battle.visible = false
+
+func _process(delta):
+	if Input.is_action_just_pressed("whitespace"):
+		if Battle.visible:
+			Battle.visible = false
+		else: 
+			Battle.visible = true
